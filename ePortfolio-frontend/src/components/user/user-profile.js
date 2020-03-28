fcb9fd882bbd175b575a173546614be291824aa7
zerhaz <m.stancelewski@gmail.com>
2020-03-28 13:37:31 +0100

    Mój Profil - Front - Tabs ver. 1

--- ePortfolio-frontend/src/components/user/user-profile.js
+++ ePortfolio-frontend/src/components/user/user-profile.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 class UserProfile extends Component {
     
     render() {
 
         return (
-            <div className="container-my">
-                <div className="user-profile-description-my">
-                    <h1>Lorem Ipsum</h1>
-                    <hr />
-                    <p>&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt diam quis gravida consectetur. Nunc ac tincidunt nisl. Mauris vestibulum, purus in laoreet ultricies, velit ante mattis felis, at gravida est est sed tellus. Nam nunc massa, maximus id nisl non, molestie viverra lectus. Quisque et nisl sed lorem semper vehicula.</p>
+            <div className="container-my container">
+                <div className="row">
+                    <div className="col-md-12 col-lg-6">
+                        {/* empty */}
+                    </div>
+
+                    <div className="col-md-12 col-lg-6 user-profile-description-my">
+                        <div className="col-md-12 page-header">
+                            <h1 className="page-title">Mój profil</h1>
+                            <hr />
+                        </div>
+                        <div className="col-md-12 page-content">
+
+                            <Tabs defaultActiveKey="user-bio" id="uncontrolled-tab-example">
+                                <Tab eventKey="user-bio" title="Dane osobowe">
+                                    <p>pierdu pierdu dodaj formularz</p>
+                                </Tab>
+                                <Tab eventKey="user-education" title="Wykształcenie" disabled >
+                                    <p>//TODO</p>
+                                </Tab>
+                                <Tab eventKey="user-experience" title="Doświadczenie" disabled>
+                                    <p>//TODO</p>
+                                </Tab>
+                            </Tabs>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- ePortfolio-frontend/src/route/header-route.js
+++ ePortfolio-frontend/src/route/header-route.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import HomeHeader from '../components/home/home-header';
+import UserHeader from '../components/user/user-header';
 
 const HeaderRoute = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={HomeHeader} />
+            <Route exact path="/moj-profil" component={UserHeader} />
             <Route path="/logowanie" />
             <Route path="/rejestracja" />
             <Route path='*' component={HomeHeader}/>

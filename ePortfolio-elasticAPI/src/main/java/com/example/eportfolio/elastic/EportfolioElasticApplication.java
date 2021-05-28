49fb1595155f43e7bcfdc4c51ec416e22600689e
zerhaz <m.stancelewski@gmail.com>
2021-05-28 17:34:23 +0200

    Elasticsearch, szukanie zaawansowane - hotfix

--- ePortfolio-elasticAPI/src/main/java/com/example/eportfolio/elastic/EportfolioElasticApplication.java
+++ ePortfolio-elasticAPI/src/main/java/com/example/eportfolio/elastic/EportfolioElasticApplication.java
@@ -2,20 +2,18 @@ package com.example.eportfolio.elastic;
 
 import com.example.eportfolio.elastic.model.AdvancedQueryHandler;
 import com.example.eportfolio.elastic.model.Person;
+import com.example.eportfolio.elastic.model.StopwordsRemover;
 import com.example.eportfolio.elastic.repository.PersonRepository;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 @SpringBootApplication
@@ -30,120 +28,130 @@ public class EportfolioElasticApplication {
 
     @PostMapping("/find/query")
     public Iterable<Person> findByQuery(@NonNull @RequestBody String queryString) throws IOException {
+        StopwordsRemover stopwordsRemover = new StopwordsRemover();
+        StringBuilder queryNoStopwords = stopwordsRemover.removeStopwords(queryString, resourceLoader);
 
-        String[] wordsArray = queryString.split(" ");
-        List<String> listOfWords = new ArrayList<String>(Arrays.asList(wordsArray));
-
-        List<String> stopwordsList = new ArrayList<String>();
-        Resource resource = resourceLoader.getResource("classpath:stopwords.txt");
-        InputStream inputStream = resource.getInputStream();
-
-        try (Scanner s = new Scanner(inputStream)) {
-            while (s.hasNext()) {
-                stopwordsList.add(s.next());
-            }
-        }
-
-        StringBuilder query = new StringBuilder();
-        for (String word : listOfWords) {
-            if (!stopwordsList.contains(word)) {
-                query.append(" ");
-                query.append(word);
-            }
-        }
-        QueryStringQueryBuilder queryBuilder = QueryBuilders.queryStringQuery(query.toString());
+        QueryBuilder queryBuilder = QueryBuilders.queryStringQuery(queryNoStopwords.toString());
         return personRepository.search(queryBuilder);
     }
 
     @PostMapping("/find/query-advanced")
     public Iterable<Person> findByQueryAdvanced(@NonNull @RequestBody AdvancedQueryHandler advancedQueryHandler) throws IOException {
-
+        StopwordsRemover stopwordsRemover = new StopwordsRemover();
         String queryText = advancedQueryHandler.getQueryText();
-        String queryCity = "/.*/";
-        String queryEduSpec = "/.*/";
-        String queryWorkIndustries = "/.*/";
-        if (!advancedQueryHandler.getQueryCity().trim().equals("")) { queryCity = advancedQueryHandler.getQueryCity(); }
-        if (!advancedQueryHandler.getQueryEduSpec().trim().equals("")) { queryEduSpec = advancedQueryHandler.getQueryEduSpec(); }
-        if (!advancedQueryHandler.getQueryWorkIndustries().trim().equals("")) { queryWorkIndustries = advancedQueryHandler.getQueryWorkIndustries(); }
-
-        String[] wordsArray = queryText.split(" ");
-        List<String> listOfWords = new ArrayList<String>(Arrays.asList(wordsArray));
-
-        List<String> stopwordsList = new ArrayList<String>();
-        Resource resource = resourceLoader.getResource("classpath:stopwords.txt");
-        InputStream inputStream = resource.getInputStream();
-
-        try (Scanner s = new Scanner(inputStream)) {
-            while (s.hasNext()) {
-                stopwordsList.add(s.next());
-            }
+        String queryCity = "";
+        String queryEduSpec = "";
+        String queryWorkIndustries = "";
+        Boolean searchInCity = false;
+        Boolean searchInEdu = false;
+        Boolean searchInWork = false;
+        if (!advancedQueryHandler.getQueryCity().trim().equals("")) {
+            searchInCity = true;
+            queryCity = advancedQueryHandler.getQueryCity();
+            queryCity = stopwordsRemover.removeStopwords(queryCity, resourceLoader).toString();
+        }
+        if (!advancedQueryHandler.getQueryEduSpec().trim().equals("")) {
+            searchInEdu = true;
+            queryEduSpec = advancedQueryHandler.getQueryEduSpec();
+        }
+        if (!advancedQueryHandler.getQueryWorkIndustries().trim().equals("")) {
+            searchInWork = true;
+            queryWorkIndustries = advancedQueryHandler.getQueryWorkIndustries();
         }
 
-        StringBuilder query = new StringBuilder();
-        for (String word : listOfWords) {
-            if (!stopwordsList.contains(word)) {
-                query.append(" ");
-                query.append(word);
-            }
+        queryText = stopwordsRemover.removeStopwords(queryText, resourceLoader).toString();
+
+        if (searchInCity && searchInEdu && searchInWork) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryCity).defaultField("city"))
+                    .must(QueryBuilders.queryStringQuery(queryEduSpec).defaultField("eduSpec"))
+                    .must(QueryBuilders.queryStringQuery(queryWorkIndustries).defaultField("workIndustries"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInCity && searchInEdu) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryCity).defaultField("city"))
+                    .must(QueryBuilders.queryStringQuery(queryEduSpec).defaultField("eduSpec"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInWork && searchInEdu) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryWorkIndustries).defaultField("workIndustries"))
+                    .must(QueryBuilders.queryStringQuery(queryEduSpec).defaultField("eduSpec"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInWork && searchInCity) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryWorkIndustries).defaultField("workIndustries"))
+                    .must(QueryBuilders.queryStringQuery(queryCity).defaultField("city"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInCity) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryCity).defaultField("city"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInEdu) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryEduSpec).defaultField("eduSpec"));
+            return personRepository.search(queryBuilder);
+        } else if (searchInWork) {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText))
+                    .must(QueryBuilders.queryStringQuery(queryWorkIndustries).defaultField("workIndustries"));
+            return personRepository.search(queryBuilder);
+        } else {
+            QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                    .must(QueryBuilders.queryStringQuery(queryText));
+            return personRepository.search(queryBuilder);
         }
 
-        QueryBuilder queryBuilder = QueryBuilders.boolQuery()
-                .must(QueryBuilders.queryStringQuery(query.toString()))
-                .must(QueryBuilders.queryStringQuery(queryCity).defaultField("city"))
-                .must(QueryBuilders.queryStringQuery(queryEduSpec).defaultField("eduSpec"))
-                .must(QueryBuilders.queryStringQuery(queryWorkIndustries).defaultField("workIndustries"));
-
-        return personRepository.search(queryBuilder);
-    }
-
-    @GetMapping("/find/all")
-    public Iterable<Person> findAllPersons() {
-        return personRepository.findAll();
     }
 
     @GetMapping("/find/city/{city}")
     public List<Person> findByCity(@PathVariable String city) {
-        return personRepository.findByCity(city);
+        return personRepository.findByCity(city.replace("-", " "));
     }
 
     @GetMapping("/find/work-name/{workNames}")
     public List<Person> findByWorkNames(@PathVariable String workNames) {
-        return personRepository.findByWorkNames(workNames);
+        return personRepository.findByWorkNames(workNames.replace("-", " "));
     }
 
     @GetMapping("/find/work-industries/{workIndustries}")
     public List<Person> findByWorkIndustries(@PathVariable String workIndustries) {
-        return personRepository.findByWorkIndustries(workIndustries);
+        return personRepository.findByWorkIndustries(workIndustries.replace("-", " "));
     }
 
     @GetMapping("/find/work-descriptions/{workDescriptions}")
     public List<Person> findByWorkDescriptions(@PathVariable String workDescriptions) {
-        return personRepository.findByWorkDescriptions(workDescriptions);
+        return personRepository.findByWorkDescriptions(workDescriptions.replace("-", " "));
     }
 
     @GetMapping("/find/edu-name/{eduNames}")
     public List<Person> findByEduNames(@PathVariable String eduNames) {
-        return personRepository.findByEduNames(eduNames);
+        return personRepository.findByEduNames(eduNames.replace("-", " "));
     }
 
     @GetMapping("/find/edu-spec/{eduSpec}")
     public List<Person> findByEduSpec(@PathVariable String eduSpec) {
-        return personRepository.findByEduSpec(eduSpec);
+        return personRepository.findByEduSpec(eduSpec.replace("-", " "));
     }
 
     @GetMapping("/find/edu-place/{eduPlace}")
     public List<Person> findByEduPlace(@PathVariable String eduPlace) {
-        return personRepository.findByEduPlace(eduPlace);
+        return personRepository.findByEduPlace(eduPlace.replace("-", " "));
     }
 
     @GetMapping("/find/edu-description/{eduDescription}")
     public List<Person> findByEduDescription(@PathVariable String eduDescription) {
-        return personRepository.findByEduDescription(eduDescription);
+        return personRepository.findByEduDescription(eduDescription.replace("-", " "));
     }
 
     @GetMapping("/find/skill-name/{skillName}")
     public List<Person> findBySkillName(@PathVariable String skillName) {
-        return personRepository.findBySkillName(skillName);
+        return personRepository.findBySkillName(skillName.replace("-", " "));
     }
 
     @PostMapping("/update")
